5f842f9f966fb4f760a0bf98e3a02b66e51028d7
vitris128 <lenhattrong128@gmail.com>
2024-03-27 18:52:32 +0700

    Update: config next-auth

--- auth.ts
+++ auth.ts
@@ -1,19 +1,67 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+
 export const authOptions = {
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {label: "Username", type: "text", placeholder: "jsmith"},
+        password: {label: "Password", type: "password"},
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("http://localhost:8080/api/v1/auths/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            // If you need to send authorization headers
+            Authorization:
+              "Basic " +
+              btoa(credentials?.username + ":" + credentials?.password),
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        });
+        // if (res.status === 401 || res.status === 400) {
+        //   return {error: "Tài khoản hoặc mật khẩu không đúng"};
+        // }
+
+        const resData = await res.json();
+        const accountInfo = resData.data.accountInfo;
+
+        const token = resData.data.token;
+
+        let user = null;
+        if (accountInfo) {
+          user = {
+            name: accountInfo.employee.fullName,
+            email: "email",
+            image: "image",
+            id: accountInfo.id,
+            username: accountInfo.username,
+            role: accountInfo.role,
+            enabled: accountInfo.enabled,
+            accessToken: resData.data.token,
+          };
+        }
+
+        if (user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
   callbacks: {
     async jwt({token, user}: {token: any; user: any}) {
       return {...token, ...user};
     },
-    async session({
-      session,
-      token,
-      user,
-    }: {
-      session: any;
-      token: any;
-      user: any;
-    }) {
-      session.user = token as any;
-
+    async session({session, token}: {session: any; token: any}) {
+      session.user = token;
       return session;
     },
   },
